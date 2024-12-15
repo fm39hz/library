eec16d9f82ca57ebfb7f9a194ae801a2f0264854
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-12-15 15:38:09 +0700

    chore: remove id in request

--- src/main/java/com/huce/library/module/user/UserRequestDto.java
+++ src/main/java/com/huce/library/module/user/UserRequestDto.java
@@ -6,7 +6,6 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class UserRequestDto {
-    private Long id;
     private String username;
     private String password;
     private String role;
